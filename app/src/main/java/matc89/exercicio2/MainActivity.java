aaeb527cd7ab205eabf5b04d99a5597345f8840c
marivaldojunior <marivaldojunior235@gmail.com>
2019-09-11 19:27:56 -0300

    Primeiro envio

--- app/src/main/java/matc89/exercicio2/MainActivity.java
+++ app/src/main/java/matc89/exercicio2/MainActivity.java
@@ -2,12 +2,19 @@ package matc89.exercicio2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button btnTrocar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void trocarUsuario (View v){
+
     }
 }
